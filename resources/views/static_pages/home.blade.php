1dc33384e6b05a2edf6fdca149756fbe3d7ae3ce
SmashHcg <673035915@qq.com>
2019-06-02 07:24:40 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello World(咋瓦鲁多!)</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">福大地图</a> 的示例项目主页。
+    </p>
+    <p>
+      在这里可以管理福大地图 App的后端用户。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">创建你的登录用户</a>
+    </p>
+  </div>
 @stop
